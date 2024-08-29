f805de89433619833bb53ca778c854a927453946
Ishitha10 <ishithabommu1@gmail.com>
2024-08-29 16:52:50 +0100

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,6 @@
+print("Ishitha")
+print(10)
+print("Ishitha", 10)
+print("I'm learning Python \n !")
+print("Hello", end=" ")
+print("World")
---
+++ main2.py
@@ -0,0 +1,6 @@
+x=input("Enter your name")
+a=int(input("Enter a number")) 
+b=int(input("Enter another number"))
+print("Your name is ", x)
+sum= a+b
+print("The answer is", sum)
